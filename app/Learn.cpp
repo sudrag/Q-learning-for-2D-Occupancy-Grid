5aeeb536d9b91b748b794db7c7307421fe5fbb30
Sudarshan raghunathan <sraghuna@terpmail.umd.edu>
2017-10-12 19:23:38 -0400

    Task#8-9:Developed function to train Q-learning and update the Q table

--- app/Learn.cpp
+++ app/Learn.cpp
@@ -22,7 +22,25 @@ int Qclass::findState(int x, int y) {
   state=x*y+y;
   return state;
 }
-
+int Qclass::findpos(int lastAction,int prev_x,int prev_y){
+	if (lastAction==0){
+	   int new_x=prev_x;
+	   int new_y=prev_y;
+	}
+	if (lastAction==1){
+		   int new_x=prev_x;
+		   int new_y=prev_y;
+		}
+	if (lastAction==2){
+		   int new_x=prev_x;
+		   int new_y=prev_y;
+		}
+	if (lastAction==3){
+		   int new_x=prev_x;
+		   int new_y=prev_y;
+		}
+	return prev_x,prev_y;
+}
 /**
  * @brief Determines the action to be performed next
  * @param int state.Current state of the robot.
@@ -53,30 +71,64 @@ int Qclass::detAction(int state) {
  * @param int previous action,int x,int y, int grid[size][size]
  * @return Returns assigned reward
  */
-int Qclass::rewardfunc(int lastAction,int x,int y,int grid[5][5]) {
+int Qclass::rewardfunc(int lastAction,int x,int y,bool restart) {
 	//Add goal condition here
   int reward;
   if(lastAction==0){
+	  if(x== goal_x && y==goal_y){
+		  reward=100;
+	  }
+	  else{
 	  if(x+1<=5)
     reward=grid[x+1][y]*-100-1;
 	  else
 		  reward=-100;
-  }
+	  restart=true;
+	  if(grid[x+1][y]==1)
+	 		  restart=true;
+	  }
+	  }
   if(lastAction==1){
+	  if(x== goal_x && y==goal_y){
+	 		  reward=100;
+	 	  }
+	 	  else{
 	  if(x-1>=0)
 	  reward=grid[x-1][y]*-100-1;
+	  else
+		  reward=-100;
+	  restart=true;
+	  if(grid[x-1][y]==1)
+	 		  restart=true;
+  }
   }
   if(lastAction==2){
+	  if(x== goal_x && y==goal_y){
+	 		  reward=100;
+	 	  }
+	 	  else{
 	  if(y+1<=5)
      reward=grid[x][y+1]*-100-1;
 	  else
 		  reward=-100;
+	      restart=true;
+	  if(grid[x][y+1]==1)
+	 		  restart=true;
+	 	  }
   }
   if(lastAction==3){
+	  if(x== goal_x && y==goal_y){
+	 		  reward=100;
+	 	  }
+	 	  else{
 	  if(y-1>=0)
      reward=grid[x][y-1]*-100-1;
 	  else
 		  reward=-100;
+	  	  restart=true;
+	  if(grid[x+1][y]==1)
+	 		  restart=true;
+	 	  }
   }
   return reward;
 }
@@ -101,21 +153,63 @@ int Qclass::futurereward(int state) {
  * @param int previous state, previous action, state, x,y
  * @return none
  */
-void Qclass::Qupdate(int lastAction, int lastState, int x,int y, int state) {
+void Qclass::Qupdate(int lastAction, int lastState, int x,int y, int state, bool restart) {
   Qclass::learn_rate=0.5;  // variables for update equation
   Qclass::disc_rew=0.8;
-  int reward=0;
-  //int reward = rewardfunc(lastAction,x,y,grid[size][size]);
+  //int reward=0;
+  int reward = Qclass::rewardfunc(lastAction,x,y,restart);
+  if(restart==true){
     Q[lastState][lastAction] = Q[lastState][lastAction] + learn_rate * (reward)
         - Q[lastState][lastAction];
+  }
+  else{
   Q[lastState][lastAction]=Q[lastState][lastAction]+ learn_rate * (reward + (disc_rew * futurereward(state))
                   - Q[lastState][lastAction]);
-}
+  }
+  }
 
 void Qclass::Train() {
-
+	int firstRun = 1;  // Can't update Q table on first run, no previous data to go off of
+	  int lastState;
+	  int lastAction;
+	  int start_x = rand % 5;
+	  int start_y = rand % 5;
+	  bool restart =false;
+	  int goal_state=findState(goal_x,goal_y);
+	  while (grid(start_x,start_y)==1){
+		  int start_x=rand % 5;
+		  int start_y=rand % 5;
+	  }
+	  int prev_x=start_x;
+	  int prev_y=start_y;
+	  for (int i = 0; i < 1000; i++) {
+	   int state = findState(start_x, start_y);
+	    while(state != goal_state){
+	    if (firstRun == 0) {
+	      prev_x,prev_y=findpos(lastAction,prev_x,prev_y);
+	      Qupdate(lastAction, lastState,prev_x,prev_y, state,restart);
+	    }
+	    int action = detAction(state);
+	    lastState = state;
+	    lastAction = action;
+	    if (restart == true) {
+	      restart = false;   // resetting so back to false
+	      firstRun = 0;
+	    }
+	  }
+}
+	  for (int i=0;i<25;i++){
+		  for (int j=0;j<4;j++){
+			  std::cout<<Q[i][j]<<"  ";
+		  }
+	  std::cout<<std::endl;
+	  }
 }
 
 void Qclass::execute() {
 
 }
+
+int main()
+{
+Qclass Q1;
