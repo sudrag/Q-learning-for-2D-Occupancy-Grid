866809f34af890f16016e80ad7a4a7b56ad67076
Sudarshan raghunathan <sraghuna@terpmail.umd.edu>
2017-10-18 00:30:12 -0400

    Task #19 : Final Travis Fix

--- gnuplot-cpp/gnuplot_i.hpp
+++ gnuplot-cpp/gnuplot_i.hpp
@@ -619,7 +619,7 @@ int Gnuplot::tmpfile_num = 0;
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__)
 #elif defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__)
 std::string Gnuplot::m_sGNUPlotFileName = "gnuplot";
-std::string Gnuplot::m_sGNUPlotPath = "/usr/local/bin";
+std::string Gnuplot::m_sGNUPlotPath = "/usr/local/bin/";
 #endif
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__)
